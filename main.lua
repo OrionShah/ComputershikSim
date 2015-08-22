6701a6800f87ad0ef4da9564afc10d0844ad58b3
OrionShah <dronshv@yandex.ru>
2015-08-23 02:41:18 +0300

    Testing with time, add ui library

--- main.lua
+++ main.lua
@@ -1,13 +1,24 @@
 debug = true
+local startTime;
+local locTime = 0;
+
+require("test")
+
+local ui = require 'ui'
 
 function love.load(arg)
-
+	  ui.registerEvents()
+	  startTime = love.timer.getTime();
 end
 
 function love.update(dt)
+
 end
 
 function love.draw(dt)
+
+	love.graphics.setBackgroundColor(255, 255, 255);
+	love.graphics.setColor(0, 0, 0);
 	-- системная инфа
 	local startX = 10;
 	local startY = 10;
@@ -23,12 +34,12 @@ function love.draw(dt)
     local x, y = love.mouse.getPosition();
     local pos = string.format("x: %d y: %d ", x, y);
 
+    time = string.format("Time from start: %d", TimeNum - startTime);
+
 	love.graphics.print(version, startX+5, startY+5);
 	love.graphics.print(os, startX+5, startY+20);
 	love.graphics.print(FPS, startX+5, startY+35);
 	love.graphics.print(Time, startX+5, startY+50);
 	love.graphics.print(pos, startX+5, startY+65);
-	--love.graphics.print("heeeey! :)", 10, 10)
-	--love.graphics.print(love.timer.getDelta(), 20, 40)
-	--love.graphics.print(love.timer.getTime(), 20, 55)
+	love.graphics.print(time, startX+5, startY+80);
 end
